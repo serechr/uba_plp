b191fc9d197964e112ccb483fc4019eca150dbd9
serechr <serenaalejandra02@gmail.com>
2025-07-27 21:15:35 -0300

    ej04 + pruebo pizarra en vs

---
+++ guía1/ej04.hs
@@ -0,0 +1,16 @@
+{-
+    Definir la función permutaciones :: [a] -> [[a]], que dada una lista devuelve todas sus permutaciones. 
+    Se recomienda utilizar concatMap :: (a -> [b]) -> [a] -> [b], y también take y drop.
+-}
+
+permutaciones :: [a] -> [[a]]
+permutaciones = undefined
+
+partes :: [a] -> [[a]]
+partes = undefined
+
+prefijos :: [a] -> [[a]]
+prefijos = undefined
+
+sublistas :: [a] -> [[a]]
+sublistas = undefined
